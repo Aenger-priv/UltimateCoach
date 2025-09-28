758a8a66d6f3a79e4645509c9dd4cecc0a7e2276
Anders Brendlien Enger <anders.enger@gmail.com>
2025-09-28 20:57:55 +0200

    Initial Commit

---
+++ UltimateCoachTests/UltimateCoachTests.swift
@@ -0,0 +1,17 @@
+//
+//  UltimateCoachTests.swift
+//  UltimateCoachTests
+//
+//  Created by Anders Enger on 28/09/2025.
+//
+
+import Testing
+@testable import UltimateCoach
+
+struct UltimateCoachTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
